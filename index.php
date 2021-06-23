f9c17df0562bd8eeeb68caa336d71c051fba4b8f
ULISES <nexu_2001@hotmail.com>
2021-06-23 13:01:05 -0500

    clase de funciones especiales para arrays con comentarios utiles

--- index.php
+++ index.php
@@ -1,52 +1,59 @@
 <?php
 
-$courses=['javascript','laravel','php', 'vuejs'];
+$courses=['php','javascript','laravel' ];
+sort($courses); //ordena de manera descendente(mayor a menor del numero de caracteres)
+//rsort($courses)  ordena de manera ascendente(menor a mayor).
+echo "<pre>";
+var_dump($courses);
+//
 
-echo "<pre>"; //"<pre>" nos hace mostyrar de manera mas clara el array que queremos mostrar.
- var_dump($courses);
-
- //cambiando el numero de item de un array
- $courses=['javascript', 10=>'laravel','php', 'vuejs'];
-
- echo "<pre>"; //"<pre>" nos hace mostyrar de manera mas clara el array que queremos mostrar.
-  var_dump($courses);
-
-  // array complejo
-
-  $courses= [
-      'frontend'=> 'javascript',
-      'framework'=> 'laravel',
-      'backend'=> 'php'
-  ];
-  /*echo "<pre>";*/
- /* var_dump($courses);*/
-  foreach ($courses as $key =>$value){  //tabien se puede poner asi: foreach ($courses as $course)    // $course en singular significa el elemento individual que esta en el array $courses
-
-      echo "$key: $value <br>";
-  };
-
-  //CON la funcion array_walk
-  $courses= [
-    'frontend'=> 'javascript',
-    'framework'=> 'laravel',
-    'backend'=> 'php'
+$courses=[
+10=>'php',
+100=>'javascript',
+1000=>'laravel' 
 ];
 
-/*function upper($course)
-{
-    echo strtoupper($course)." <br>";
-}*/
+ksort($courses);//ordena las llaves de menor a mayor
+//krsort($courses);// ordena de forma inversa(mayor a menor)
+echo "<pre>";
+//var_dump($courses);
 
-function upper($course,$key)
-{
-    echo strtoupper($course).": $key <br>";
-}
+var_dump(array_slice($courses,1));//corta el primer elemento y te muestra lo restante
+
+//array chunk
+$courses=['php','javascript','laravel' ];
+echo "<pre>";
+var_dump(array_chunk($courses,2));//array_chunk: Divide un array en fragmentos(otros arrays).
+// array_shift($courses)
+$courses=['php','javascript','laravel' ];
+echo "<pre>";
+var_dump(array_shift($courses));// elimina el primer elemento del array original y muestra el elemento eleiminado en pantalla.
+var_dump($courses);
+
+//array_pop($courses)
+$courses=['php','javascript','laravel' ];
+echo "<pre>";
+var_dump(array_pop($courses));//elimina el utimo elelemento del array original y lo muestra en pantalla
+
+//array_unshift()
+$courses=['php','javascript','laravel' ];
+echo "<pre>";
+var_dump(array_unshift($courses,'emacscript')); //aumenta uno o mas elelemtos al inicio array original
+var_dump($courses);
+
+//array_push
+$courses=['php','javascript','laravel' ];
+echo "<pre>";
+var_dump(array_push($courses,'python'));// aumenta uno o mas elelmentos al final del array original
+var_dump($courses);
+
+//array_flip
+$courses=[
+'php'=> 'backend',
+'javascript'=> 'frontend',
+'laravel' => 'framework'
+];
+echo "<pre>";
+var_dump(array_flip($courses));//intercambia el valor de la key por el value de todo el contenido del array
 
 
-array_walk($courses,'upper');
-
-//otras funciones que son importantes
-/*array_key_exists('frontend', $courses); //te saldra en la pantalla "true " ya que la función busca en tu array si existe ese value.
-/*in_array('javascript','$courses'); // esta función hace la busqueda de valores(values)
-/array_keys($courses) // imprime los keys en pantalla.
-/array_values($courses)//imprime todos los valores de tu array.
